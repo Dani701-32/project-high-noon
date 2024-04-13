ed6cf228ec5aa43e1aede26186af29fbe052ef75
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2024-04-13 18:42:08 -0300

    Change player color
    Change player and flags color based on the team data scriptable object

--- Assets/TEMP_ASSETS/Scripts/TEMP_PlayerStats.cs
+++ Assets/TEMP_ASSETS/Scripts/TEMP_PlayerStats.cs
@@ -1,18 +1,34 @@
-
 using UnityEngine;
 
 public class TEMP_PlayerStats : MonoBehaviour
 {
-    [Header("Team data")] 
-    [SerializeField] TeamData team;
+    [Header("Team data")]
+    [SerializeField]
+    TeamData team;
+
     [Header("Toggleable objects")]
-    [SerializeField] GameObject flagCarryObject;
-    [SerializeField] GameObject flagCarryEffects;
+    [SerializeField]
+    GameObject flagCarryObject;
+
+    [SerializeField]
+    GameObject flagCarryEffects;
+
     //public int HP = 100;
+    [SerializeField]
+    GameObject model;
 
     //private int HPMax = 100;
     [SerializeField, ReadOnly]
     bool _hasFlag;
+
+    private void Start()
+    {
+        if (team != null)
+        {
+            model.GetComponent<MeshRenderer>().material = team.teamEquipMaterial;
+        }
+    }
+
     public bool hasFlag
     {
         get => _hasFlag;
@@ -28,7 +44,10 @@ public class TEMP_PlayerStats : MonoBehaviour
 
     private void FlagUpdate()
     {
-        if(flagCarryObject != null) flagCarryObject.SetActive(!hasFlag);
-        if(flagCarryEffects != null) flagCarryEffects.SetActive(!hasFlag);
+        if (flagCarryObject != null)
+            flagCarryObject.SetActive(!hasFlag);
+        if (flagCarryEffects != null)
+            flagCarryEffects.SetActive(!hasFlag);
+        flagCarryObject.GetComponent<MeshRenderer>().material.color = team.teamColor;
     }
 }
